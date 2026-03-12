64bd365ecbffeba96ee83a49ae60ae9ec793faf2
mauromanuek <mauromkmanuel@gmail.com>
2026-03-12 01:30:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/Master-bot-/'})
   plugins: [react()],
-  base: './',
+
+  base: '/Master-bot-/',
+
   server: {
     port: 5173,
     host: true
   },
+
   build: {
     outDir: 'dist',
     sourcemap: false,
