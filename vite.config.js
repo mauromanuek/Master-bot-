49e1671b78c4956a30871fcab91f50547cfb21be
mauromanuek <mauromkmanuel@gmail.com>
2026-03-12 01:55:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  base: '/Master-bot-/',
-
+  // CORREÇÃO: Isso diz ao Vite que o site está em uma subpasta
+  base: '/Master-bot-/', 
   server: {
     port: 5173,
     host: true
   },
-
   build: {
     outDir: 'dist',
     sourcemap: false,
